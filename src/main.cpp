9390bd44c49b41444e5cfaea2d76a54fd320a8f7
MolkovAlexandr <163853240+MolkovAlexandr@users.noreply.github.com>
2024-04-25 16:23:45 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,13 @@
 int main() {
 setlocale(LC_ALL, "1251");
 
-const char* s1 = 
+const char* s1 =
 "один д2ва три че4тыре пят5ь шесть сем7ь восемь девять дес10ять";
 
-const char* s2 = 
+const char* s2 =
 "One tWo THree FoUr fIve siX SevEn eIght NiNe Ten";
 
-const char* s3 = 
+const char* s3 =
 "ah2 4a asf2u5 ac0psv4 cn132 9fs13la afj2 ac3vk";
 
 std::cout << "функция 1 вернула значение " << faStr1(s1) << std::endl;
